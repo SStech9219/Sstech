6e76766783b0e75e1c24e5f3605c95bed480aa41
Anek9219 <anekchauhan01@gmail.com>
2024-12-23 18:46:41 +0530

    first commit

---
+++ backend/Source/routes/emailRoute.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const { sendEmail } = require('../utils/mail');
+
+// POST route to handle form submissions
+router.post('/', async (req, res) => {
+  const { name, email, phone, service, website, message } = req.body;
+
+  try {
+    await sendEmail({ name, email, phone, service, website, message });
+    res.status(200).json({ message: 'Email sent successfully!' });
+  } catch (error) {
+    console.error('Error sending email:', error);
+    res.status(500).json({ message: 'Failed to send email.' });
+  }
+});
+
+module.exports = router;
+
---
+++ backend/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors'); // Import cors
+const applyMiddleware = require('./Source/middleware/commonMiddleware');
+const emailRoute = require('./Source/routes/emailRoute');
+const mainformroute = require('./Source/routes/mainFormRoute')
+const jobformroute = require('./Source/routes/JobFormRoute')
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Enable CORS
+app.use(cors({
+  origin: ['https://offdiary.in', 'http://localhost:5173'], // Allow only your frontend domain
+    methods: 'GET, POST, PUT, DELETE', // Allowed HTTP methods
+    allowedHeaders: 'Content-Type', // Allowed headers
+  }));
+
+// Apply common middleware
+applyMiddleware(app);
+
+// Use email route
+app.use('/api/send-email', emailRoute); 
+app.use('/api/receive-msg',mainformroute)
+app.use('/api/users',jobformroute)
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+
+
+
+
+
+
+
+
